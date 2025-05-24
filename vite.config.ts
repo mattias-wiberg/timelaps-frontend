9871ad0a3e9a76ba18056bd50f0505152f1aa7f6
Mattias Wiberg <mattiaswiberg@outlook.com>
2025-05-24 16:03:08 +0200

    Add docker files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/timelaps/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
